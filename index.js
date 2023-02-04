15f2151a8a70c43bcc0562b545f542716d922700
mabdullah <mabdullah2037@gmail.com>
2023-02-04 10:18:48 +0500

    cors added

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose');
 const app = express();
 const routes = require('./routes/routes')
 
+var cors = require('cors')
+
+app.use(cors()) 
+
 app.use(express.json())
 app.use((req, res, next)=>{
     console.log(req.path , req.method);
